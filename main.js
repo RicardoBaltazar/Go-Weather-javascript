c27b084e0f2f07390436c11ee5f9ec572f07a511
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-05-26 19:50:48 -0300

    repaginando projeto
    promeiras atualizações feitas

--- main.js
+++ main.js
@@ -1,98 +1,55 @@
+document.querySelector('.blockWeather').style.display = 'none'
 
-var res = document.querySelector('#res')
-var form = document.querySelector('form')
-var clima = {}
+document.querySelector('#btn').addEventListener('click',function(e){
+        e.preventDefault()
+        
+        var city = document.querySelector('#search')
 
-document.querySelector(".block").style.display = "none";
+        document.querySelector('.blockWeather').style.display = 'block'
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    
-    
-    var nameCity = document.querySelector('input')
-    
-    
-    const options = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default'
-    }
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${nameCity.value}&appid=17c6ff7a68ddf23b9c99f11ba7456398&units=metric`, options)
-    
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-        console.log(data.name)
-        console.log(data.sys.country)
-        console.log(data.main.temp)
-        console.log(data.weather[0].icon)
-        console.log(data.weather[0].description)
-
-        document.querySelector(".block").style.display = "block";
         
-        clima.name = data.name
-        clima.country = data.sys.country
-        clima.temp = data.main.temp
-        clima.icon = data.weather[0].icon
-        clima.description = data.weather[0].description
+        const options = {
+                method: 'GET',
+                mode: 'cors',
+                cache: 'default'
+        }
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=17c6ff7a68ddf23b9c99f11ba7456398&units=metric`, options)
         
+        .then(function (response) {
+                return response.json();
+        })
+        .then(function (data) {
+                console.log(data)
+                console.log(data.name)
+                console.log(data.sys.country)
+                console.log(data.main.temp)
+                console.log(data.weather[0].description)
+                console.log(data.main.temp_min)
+                console.log(data.main.temp_max)
+                console.log(data.main.feels_like)
+                console.log(data.main.humidity)
+                console.log(data.wind.speed)
+                
+                
+                document.querySelector('#country').innerHTML = data.name + ' - ' + data.sys.country
+                document.querySelector('#temp').innerHTML = data.main.temp + ' ' + data.weather[0].description
+                document.querySelector('#min').innerHTML = data.main.temp_min + 'º'
+                document.querySelector('#max').innerHTML = data.main.temp_max + 'º'
+                document.querySelector('#feelsLike').innerHTML = data.main.feels_like + 'º'
+                document.querySelector('#wind').innerHTML = data.wind.speed + 'km/h'
+                document.querySelector('#humidity').innerHTML = data.main.humidity + '%'
+                
+                
+        })
+        .catch(function (e) {
+                console.log('Erro: ')
+        })
         
-        document.querySelector('#res1').innerHTML = clima.name
-        document.querySelector('#res2').innerHTML =  clima.country         
-        document.querySelector('#res3').innerHTML = clima.temp + '°C'
-        document.querySelector('#res5').innerHTML =  clima.description
-        
-        
-        
-        if(clima.icon == '01n'){
-            document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/01n@2x.png">'
-        } else if(clima.icon == '01d'){
-                document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/01d@2x.png">'
-            } else if(clima.icon == '02d'){
-                document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/02d@2x.png">'
-            } else if(clima.icon == '02n'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/02n@2x.png">'
-            } else if(clima.icon== '03d'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/03d@2x.png">'
-            } else if(clima.icon =='03n'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/03n@2x.png">'
-            } else if(clima.icon == '04d'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/04d@2x.png">'
-            } else if(clima.icon == '04n'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/04n@2x.png">'
-            } else if(clima.icon == '09d'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/09d@2x.png">'
-            } else if(clima.icon == '09n'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/09n@2x.png">'
-            } else if (clima.icon == '10d'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/10d@2x.png">'
-            } else if(clima.icon == '10n'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/10n@2x.png">'
-            } else if (clima.icon == '11d'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/11d@2x.png">'
-            } else if (clima.icon == '11n'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/11n@2x.png">'
-            } else if(clima.icon == '13d'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/13d@2x.png">'
-            } else if(clima.icon == '13n'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/13n@2x.png">'
-            } else if(clima.icon == '50d'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/50d@2x.png">'
-            } else if (clima.icon == '50n'){
-                    document.querySelector('#res4').innerHTML = '<img src="http://openweathermap.org/img/wn/50d@2x.png">'
-            }
-        
-    })
-    .catch(function(e){
-        console.log('Erro: '+ e,message)
-    })  
-
-    nameCity.value = ''
 })
-    
 
-    
-    
-    
+
+
+
+
+
+
