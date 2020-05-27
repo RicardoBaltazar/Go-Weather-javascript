7776da8b2454faf7b18ef8c35f257bc7cecbdad4
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-05-26 23:10:03 -0300

    atualizações realizadas

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ document.querySelector('#btn').addEventListener('click',function(e){
         var city = document.querySelector('#search')
 
         document.querySelector('.blockWeather').style.display = 'block'
+        document.querySelector('.blockWeather').style.display = 'flex'
 
         
         const options = {
@@ -30,22 +31,34 @@ document.querySelector('#btn').addEventListener('click',function(e){
                 console.log(data.main.humidity)
                 console.log(data.wind.speed)
                 
-                
+                var temp = data.main.temp
+                parseFloat(temp.value)
+
+                var tempMin = data.main.temp_min
+                parseFloat(tempMin.value)
+
+                var tempMax = data.main.temp_max
+                parseFloat(tempMax.value)
+
+                var feelsLike = data.main.feels_like
+                parseFloat(feelsLike.value)
+
                 document.querySelector('#country').innerHTML = data.name + ' - ' + data.sys.country
-                document.querySelector('#temp').innerHTML = data.main.temp + ' ' + data.weather[0].description
-                document.querySelector('#min').innerHTML = data.main.temp_min + 'º'
-                document.querySelector('#max').innerHTML = data.main.temp_max + 'º'
-                document.querySelector('#feelsLike').innerHTML = data.main.feels_like + 'º'
+                document.querySelector('#temp').innerHTML = temp.toFixed() + 'ºC ' + data.weather[0].description
+                document.querySelector('#min').innerHTML = temp.toFixed() + 'ºC'
+                document.querySelector('#max').innerHTML = tempMax.toFixed() + 'ºC'
+                document.querySelector('#feelsLike').innerHTML = feelsLike.toFixed() + 'ºC'
                 document.querySelector('#wind').innerHTML = data.wind.speed + 'km/h'
                 document.querySelector('#humidity').innerHTML = data.main.humidity + '%'
-                
                 
         })
         .catch(function (e) {
                 console.log('Erro: ')
         })
         
+        city.value = ''
 })
+
 
 
 
