93f127c9675cdcf047c563651fa78b54adf61eb6
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-06-26 13:31:05 -0300

    criada consulta para cidades

--- scripts/capitals.js
+++ scripts/capitals.js
@@ -1,20 +1,5 @@
-/*
-let spanList = [] 
-spanList.saoPaulo = document.querySelector('#sp')
-spanList.london = document.querySelector('#london')
-capitals.paris = document.querySelector('#paris')
-capitals.dublin = document.querySelector('#dublin')
-capitals.tokyo = document.querySelector('#tokyo')
-capitals.otawa = document.querySelector('#otawa')
-capitals.berlim = document.querySelector('#berlim')
-capitals.amsterdam = document.querySelector('#amsterdam')
-capitals.copenhague = document.querySelector('#copenhague')
-capitals.reykjavik = document.querySelector('#reykjavik')
-*/
-
-
 const capitals = []
-capitals.saoPaulo = 'sao paulo'
+capitals.Estocolmo = 'Estocolmo'
 capitals.london = 'London'
 capitals.paris = 'Paris'
 capitals.dublin = 'Dublin'
@@ -26,21 +11,29 @@ capitals.copenhague = 'Copenhague'
 capitals.reykjavik = 'Reykjavik'
 
 for(indice in capitals){
-    
+
     const options = {
         method: 'GET',
         mode: 'cors',
         cache: 'default'
     }
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=$&appid=17c6ff7a68ddf23b9c99f11ba7456398&units=metric`, options)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${indice}&appid=17c6ff7a68ddf23b9c99f11ba7456398&units=metric`, options)
     
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         //console.log(data)
-        console.log(data.name)
+        //console.log(data.name)
         //console.log(data.main.temp)
+        let namesCities = []
+        namesCities = data.name
+        console.log(namesCities)
+
+        let tempCities = []
+        tempCities = data.main.temp
+        console.log(tempCities)
+        document.querySelector('.capitals').innerHTML += "<li><span>" + tempCities.toFixed() + 'º ' +"</spam>" + namesCities + "</li>"
     })
 
 }
